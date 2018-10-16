0ef499accdf498cfa9099e75322d77c075412e1b
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-10-17 02:39:44 +0300

    Integrate Pest: part 2

--- step1/src/parse.rs
+++ step1/src/parse.rs
@@ -46,24 +46,74 @@ pub mod ast {
     #[pest(rule = "Rule::quote")]
     pub struct Quote<'i> {
         pub span: Span<'i>,
-        pub inner: Obj<'i>,
+        pub inner: Box<Obj<'i>>,
     }
 
-/*
-    | round
-    | square
-    | curly
-    | withmeta
-    | quote
-    | quasiquote
-    | unquote
-    | spliceunquote
-*/
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::quasiquote")]
+    pub struct Quasiquote<'i> {
+        pub span: Span<'i>,
+        pub inner: Box<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::unquote")]
+    pub struct Unquote<'i> {
+        pub span: Span<'i>,
+        pub inner: Box<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::spliceunquote")]
+    pub struct Spliceunquote<'i> {
+        pub span: Span<'i>,
+        pub inner: Box<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::withmeta")]
+    pub struct Withmeta<'i> {
+        pub span: Span<'i>,
+        pub meta: Box<Obj<'i>>,
+        pub inner: Box<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::round")]
+    pub struct Round<'i> {
+        pub span: Span<'i>,
+        pub items: Vec<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::square")]
+    pub struct Square<'i> {
+        pub span: Span<'i>,
+        pub items: Vec<Obj<'i>>,
+    }
+
+    #[derive(Debug, FromPest)]
+    #[pest(rule = "Rule::curly")]
+    pub struct Curly<'i> {
+        pub span: Span<'i>,
+        pub items: Vec<Obj<'i>>,
+    }
 
     #[derive(Debug, FromPest)]
     #[pest(rule = "Rule::obj")]
     pub enum Obj<'i> {
+        Round(Round<'i>),
+        Square(Square<'i>),
+        Curly(Curly<'i>),
         Int(Int<'i>),
+        Ident(Ident<'i>),
+        Keyword(Keyword<'i>),
+        Atom(Atom<'i>),
         StrLit(StrLit<'i>),
+        Quote(Quote<'i>),
+        Quasiquote(Quasiquote<'i>),
+        Unquote(Unquote<'i>),
+        Spliceunquote(Spliceunquote<'i>),
+        Withmeta(Withmeta<'i>),
     }
 }
