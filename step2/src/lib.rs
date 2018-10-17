2cc90b9735d2e51aa73f64d4f977d16053680c43
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-10-17 16:34:43 +0300

    Functions: -,/,*

--- step2/src/lib.rs
+++ step2/src/lib.rs
@@ -87,6 +87,39 @@ pub mod stdlib {
         };
         Ok(Ast::Int(sum))
     }
+    pub fn minus(x: &[Ast]) -> Result<Ast> {
+        match x.len() {
+            1 => match x[0].ignoremeta() {
+                    Ast::Int(n) => Ok(Ast::Int(-n)),
+                    _ => bail!("- does not support this type"),
+                },
+            2 => match (x[0].ignoremeta(), x[1].ignoremeta()) {
+                    (Ast::Int(n),Ast::Int(v)) => Ok(Ast::Int(n-v)),
+                    _ => bail!("- does not support this type"),
+                },
+            _ => bail!("- must have exactly 1 or 2 arguments"),
+        }
+    }
+    pub fn times(x: &[Ast]) -> Result<Ast> {
+        let mut prod = 1;
+        for i in x {
+            match i.ignoremeta() {
+                Ast::Int(n) => prod*=n,
+                _ => bail!("* does not support this type"),
+            }
+        };
+        Ok(Ast::Int(prod))
+    }
+    pub fn divide(x: &[Ast]) -> Result<Ast> {
+        match x.len() {
+            2 => match (x[0].ignoremeta(), x[1].ignoremeta()) {
+                    (Ast::Int(_),Ast::Int(0)) => bail!("division by zero"),
+                    (Ast::Int(n),Ast::Int(v)) => Ok(Ast::Int(n/v)),
+                    _ => bail!("/ does not support this type"),
+                },
+            _ => bail!("/ must have exactly 2 arguments"),
+        }
+    }
 }
 
 impl Malvi {
@@ -96,6 +129,9 @@ impl Malvi {
         };
         this.binding.insert("id".to_string(), Box::new(stdlib::id));
         this.binding.insert("+".to_string(), Box::new(stdlib::plus));
+        this.binding.insert("-".to_string(), Box::new(stdlib::minus));
+        this.binding.insert("*".to_string(), Box::new(stdlib::times));
+        this.binding.insert("/".to_string(), Box::new(stdlib::divide));
         this
     }
 }
