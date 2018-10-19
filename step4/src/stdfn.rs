0edea864bb899a23bbf026ba3219368b75c6be9f
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-10-20 00:57:41 +0300

    Cut some obvious boilerplate

--- step4/src/stdfn.rs
+++ step4/src/stdfn.rs
@@ -1,9 +1,16 @@
-use super::{Result,Ast,SAst,Malvi,Bindings,BindingsHandle};
-use ::std::rc::Rc;
-use ::std::cell::RefCell;
+use super::{Ast, Bindings, BindingsHandle, Malvi, Result, SAst};
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub fn nimpl(_:&mut Malvi, _:&BindingsHandle, _: &[Rc<Ast>]) -> Result<Ast> {
-   bail!("Not implemented")
+pub fn nimpl(_: &mut Malvi, _: &BindingsHandle, _: &[Rc<Ast>]) -> Result<Ast> {
+    bail!("Not implemented")
+}
+
+macro_rules! Int {
+    ($($x:tt)*) => {Ast::Simple(SAst::Int($($x)*))};
+}
+macro_rules! Sym {
+    ($($x:tt)*) => {Ast::Simple(SAst::Symbol($($x)*))};
 }
 
 impl Malvi {
@@ -14,21 +21,30 @@ impl Malvi {
             ($n:expr) => {{
                 let s = this.sym($n);
                 let b = this.builtins.insert(Rc::new(nimpl));
-                this.root_bindings.borrow_mut().at_this_level.insert(s, Ast::BuiltinMacro(b));
+                this.root_bindings
+                    .borrow_mut()
+                    .at_this_level
+                    .insert(s, Ast::BuiltinMacro(b));
             }};
         }
         macro_rules! builtin_func {
             ($n:expr, $f:expr) => {{
                 let s = this.sym($n);
                 let b = this.builtins.insert(Rc::new($f));
-                this.root_bindings.borrow_mut().at_this_level.insert(s, Ast::BuiltinFunction(b));
+                this.root_bindings
+                    .borrow_mut()
+                    .at_this_level
+                    .insert(s, Ast::BuiltinFunction(b));
             }};
         }
         macro_rules! builtin_macro {
             ($n:expr, $f:expr) => {{
                 let s = this.sym($n);
                 let b = this.builtins.insert(Rc::new($f));
-                this.root_bindings.borrow_mut().at_this_level.insert(s, Ast::BuiltinMacro(b));
+                this.root_bindings
+                    .borrow_mut()
+                    .at_this_level
+                    .insert(s, Ast::BuiltinMacro(b));
             }};
         }
 
@@ -36,123 +52,110 @@ impl Malvi {
         builtin_notimpl_macro!("unquote");
         builtin_notimpl_macro!("splice-unquote");
         builtin_notimpl_macro!("deref");
-        
-        builtin_macro!("quote", |m,_env,x|{
-            let mut v = vec![Rc::new(Ast::Simple(SAst::Symbol(m.sym("quote"))))];
+
+        builtin_macro!("quote", |m, _env, x| {
+            let mut v = vec![Rc::new(Sym!(m.sym("quote")))];
             v.extend_from_slice(x);
             Ok(Ast::Round(v))
         });
 
-        builtin_macro!("with-meta",|_,_,x|{
-            if x.len() == 2 {
-                Ok((*x[0]).clone())
-            } else {
-                bail!("with-meta macro must have exactly two arguments")
-            }            
+        builtin_macro!("with-meta", |_, _, x| if x.len() == 2 {
+            Ok((*x[0]).clone())
+        } else {
+            bail!("with-meta macro must have exactly two arguments")
         });
 
-        builtin_func!("id", |_,_,x| {
-            if x.len() == 1 {
-                Ok((*x[0]).clone())
-            } else {
-                bail!("id funciton must have exactly one argument")
-            }
+        builtin_func!("id", |_, _, x| if x.len() == 1 {
+            Ok((*x[0]).clone())
+        } else {
+            bail!("id funciton must have exactly one argument")
         });
 
-        builtin_func!("+" , |_,_,x|{
+        builtin_func!("+", |_, _, x| {
             let mut sum = 0;
             for i in x {
                 match **i {
-                    Ast::Simple(SAst::Int(n)) => sum+=n,
+                    Int!(n) => sum += n,
                     _ => bail!("+ does not support this type"),
                 }
-            };
-            Ok(Ast::Simple(SAst::Int(sum)))            
+            }
+            Ok(Int!(sum))
         });
 
-        builtin_func!("-" , |_,_,x|{
-            match x.len() {
-                1 => match *x[0] {
-                    Ast::Simple(SAst::Int(n)) => Ok(Ast::Simple(SAst::Int(-n))),
-                        _ => bail!("- does not support this type"),
-                    },
-                2 => match (&*x[0], &*x[1]) {
-                        (Ast::Simple(SAst::Int(n)),Ast::Simple(SAst::Int(v))) => Ok(Ast::Simple(SAst::Int(n-v))),
-                        _ => bail!("- does not support this type"),
-                    },
-                _ => bail!("- must have exactly 1 or 2 arguments"),
-            }            
+        builtin_func!("-", |_, _, x| match x.len() {
+            1 => match *x[0] {
+                Int!(n) => Ok(Int!(-n)),
+                _ => bail!("- does not support this type"),
+            },
+            2 => match (&*x[0], &*x[1]) {
+                (Int!(n), Int!(v)) => Ok(Int!(n - v)),
+                _ => bail!("- does not support this type"),
+            },
+            _ => bail!("- must have exactly 1 or 2 arguments"),
         });
 
-        builtin_func!("*" , |_,_,x|{
+        builtin_func!("*", |_, _, x| {
             let mut prod = 1;
             for i in x {
                 match **i {
-                    Ast::Simple(SAst::Int(n)) => prod*=n,
+                    Int!(n) => prod *= n,
                     _ => bail!("* does not support this type"),
                 }
-            };
-            Ok(Ast::Simple(SAst::Int(prod)))
-        });
-
-        builtin_func!("/" , |_,_,x|{
-            match x.len() {
-                2 => match (&*x[0], &*x[1]) {
-                        (Ast::Simple(SAst::Int(_)),Ast::Simple(SAst::Int(0))) => bail!("division by zero"),
-                        (Ast::Simple(SAst::Int(n)),Ast::Simple(SAst::Int(v))) => Ok(Ast::Simple(SAst::Int(n/v))),
-                        _ => bail!("/ does not support this type"),
-                    },
-                _ => bail!("/ must have exactly 2 arguments"),
             }
+            Ok(Int!(prod))
         });
 
-        builtin_macro!("def!" , |m,env,x|{
-            match x.len() {
-                2 => match (&*x[0], &*x[1]) {
-                        (Ast::Simple(SAst::Symbol(n)),v) => {
-                            let vv = m.eval_impl(env, &v)?;
-                            env.borrow_mut().at_this_level.insert(*n, vv.clone());
-                            Ok(vv)
-                        },
-                        _ => bail!("First argument of set! must be a symbol"),
-                    },
-                _ => bail!("set! must have exactly 2 arguments"),
-            }            
+        builtin_func!("/", |_, _, x| match x.len() {
+            2 => match (&*x[0], &*x[1]) {
+                (Int!(_), Int!(0)) => bail!("division by zero"),
+                (Int!(n), Int!(v)) => Ok(Int!(n / v)),
+                _ => bail!("/ does not support this type"),
+            },
+            _ => bail!("/ must have exactly 2 arguments"),
         });
 
-        builtin_macro!("let*" , let_);
+        builtin_macro!("def!", |m, env, x| match x.len() {
+            2 => match (&*x[0], &*x[1]) {
+                (Sym!(n), v) => {
+                    let vv = m.eval_impl(env, &v)?;
+                    env.borrow_mut().at_this_level.insert(*n, vv.clone());
+                    Ok(vv)
+                }
+                _ => bail!("First argument of set! must be a symbol"),
+            },
+            _ => bail!("set! must have exactly 2 arguments"),
+        });
+
+        builtin_macro!("let*", let_);
     }
 }
 
-
-pub fn let_(m:&mut Malvi, env:&BindingsHandle, x: &[Rc<Ast>]) -> Result<Ast> {
+pub fn let_(m: &mut Malvi, env: &BindingsHandle, x: &[Rc<Ast>]) -> Result<Ast> {
     match x.len() {
         2 => match (&*x[0], &*x[1]) {
-                | (Ast::Round(n),v) 
-                | (Ast::Square(n),v)
-                => {
-                    if n.len() % 2 != 0 {
-                        bail!("Odd number of elements in bindings list")
-                    }
-                    let mut new_bindings = Bindings {
-                        at_this_level: ::std::collections::HashMap::new(),
-                        parent: Some(env.clone()),
-                    };
-                    let bh = Rc::new(RefCell::new(new_bindings));
-                    for bind in n[..].chunks_exact(2) {
-                        match (&*bind[0],&*bind[1]) {
-                            (Ast::Simple(SAst::Symbol(s)), v) => {
-                                let vv = m.eval_impl(&bh, v)?;
-                                bh.borrow_mut().at_this_level.insert(*s, vv);
-                            },
-                            _ => bail!("Non-symbol speficied to let* for binding")
+            (Ast::Round(n), v) | (Ast::Square(n), v) => {
+                if n.len() % 2 != 0 {
+                    bail!("Odd number of elements in bindings list")
+                }
+                let mut new_bindings = Bindings {
+                    at_this_level: ::std::collections::HashMap::new(),
+                    parent: Some(env.clone()),
+                };
+                let bh = Rc::new(RefCell::new(new_bindings));
+                for bind in n[..].chunks_exact(2) {
+                    match (&*bind[0], &*bind[1]) {
+                        (Sym!(s), v) => {
+                            let vv = m.eval_impl(&bh, v)?;
+                            bh.borrow_mut().at_this_level.insert(*s, vv);
                         }
+                        _ => bail!("Non-symbol speficied to let* for binding"),
                     }
-                    let vv = m.eval_impl(&bh, &v)?;
-                    Ok(vv)
-                },
-                _ => bail!("First argument of set! must be square or round brackets"),
-            },
+                }
+                let vv = m.eval_impl(&bh, &v)?;
+                Ok(vv)
+            }
+            _ => bail!("First argument of set! must be square or round brackets"),
+        },
         _ => bail!("let* must have exactly 2 arguments"),
     }
 }
