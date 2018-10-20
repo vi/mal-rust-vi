157cb4687fe710aace6ee7b4853a64d016da9aec
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-10-21 00:13:02 +0300

    Non-bool if

--- step4/src/stdfn_part2.rs
+++ step4/src/stdfn_part2.rs
@@ -27,18 +27,32 @@ impl Malvi {
             Ast::Curly(x) => Ast::Simple(SAst::Bool(x.is_empty())),
             _ => bail!("Can't check emptiness of this"),
         }));
-        builtin_macro!("if", |m,env_,mut x:Vector<Rc<Ast>>| {
+        builtin_macro!("if", |m,env,mut x:Vector<Rc<Ast>>| {
             if x.len() != 3 {
                 bail!("`if` has exactly three arguments");
             }
             let cond = x.pop_front().unwrap();
             let iftrue = x.pop_front().unwrap();
             let iffalse = x.pop_front().unwrap();
-            let env = env_.clone();
-            match m.eval_impl(env_,&cond)? {
-                True!() => Ok(Ast::EvalMeAgain{obj:iftrue, env}),
-                False!() => Ok(Ast::EvalMeAgain{obj:iffalse, env}),
-                _ => bail!("Non-boolean in `if` conditional"),
+            let iftrue = ||Ok(Ast::EvalMeAgain{obj:iftrue, env:env.clone()});
+            let iffalse = ||Ok(Ast::EvalMeAgain{obj:iffalse, env:env.clone()});
+            match m.eval_impl(env,&cond)? {
+                True!() => iftrue(),
+                False!() => iffalse(),
+                Int!(x) if x == 0 => iffalse(),
+                Int!(_) => iftrue(),
+                Ast::Simple(SAst::Nil) => iffalse(),
+                | Ast::Round(ref x) 
+                | Ast::Square(ref x)
+                if x.is_empty() => iffalse(),
+                | Ast::Round(ref x) 
+                | Ast::Square(ref x)
+                => iftrue(),
+                Ast::Curly(ref x) if x.is_empty() => iffalse(),
+                Ast::Curly(ref x) => iftrue(),
+                Ast::Simple(SAst::StrLit(ref x)) if x.is_empty() => iffalse(),
+                Ast::Simple(SAst::StrLit(ref x)) => iftrue(),
+                _ => bail!("Wrong type used in `if` conditional"),
             }
         });
     }
