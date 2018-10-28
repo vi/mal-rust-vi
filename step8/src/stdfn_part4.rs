e2a19275f546f0cf3c8d00796ab6ff9d83ea3fe2
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-10-28 04:07:27 +0300

    nth

--- step8/src/stdfn_part4.rs
+++ step8/src/stdfn_part4.rs
@@ -1,6 +1,5 @@
-use super::{Ast, Malvi, SAst, UserFunction, BindingsHandle};
+use super::{Ast, Malvi, SAst};
 use std::rc::Rc;
-use crate::im::Vector;
 
 impl Malvi {
     pub fn stdfn_part4(&mut self) {
@@ -34,6 +33,28 @@ impl Malvi {
                 },
             _ => bail!("rest does not support this type"),
         }));
+
+        builtin_func2!("nth", |_,_,list:Rc<Ast>,idx:Rc<Ast>| Ok({
+            let mut idx = match &*idx {
+                Int!(n) => *n,
+                _ => bail!("Second argument of nth must be an int")
+            };
+            match &*list {
+                | Ast::Round(x) 
+                | Ast::Square(x)
+                => {
+                    if idx < 0 {
+                        idx = (x.len() as i64) + idx;
+                    };
+
+                    if let Some(v) = x.get(idx as usize) {
+                        (**v).clone()
+                    } else {
+                        bail!("Index out of range")
+                    }
+                },
+                _ => bail!("nth first argument invalid type"),
+        }}));
     }
 }
 
