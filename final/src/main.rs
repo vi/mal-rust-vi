ed479464a5279bdfcda5fb293b03bdaf09d10e33
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-11-09 16:11:31 +0300

    Improve CLI

--- final/src/main.rs
+++ final/src/main.rs
@@ -4,19 +4,18 @@ extern crate malvi_final as malvi;
 
 use self::malvi::{Malvi,Mal};
 
-fn main() -> malvi::Result<()> {
+fn interactive_mode<T:Mal>(m:&mut T) -> malvi::Result<()> {
     let mut line = "".to_string();
-    let mut p = Malvi::new();
     loop {
         line.clear();
         if 0 == ::std::io::stdin().read_line(&mut line)? {
             break;
         }
         let ret : malvi::Result<()> = try {
-            let i = p.read(&line)?;
+            let i = m.read(&line)?;
             for xx in i {
-                let o = p.eval(&xx)?;
-                line = p.print(&o)?;
+                let o = m.eval(&xx)?;
+                line = m.print(&o)?;
                 println!(";=>{}", line);
             }
             println!();
@@ -26,5 +25,53 @@ fn main() -> malvi::Result<()> {
             eprintln!("error: {}", e);
         }
     };
+    Ok(())
+}
+
+fn main() -> malvi::Result<()> {
+    let mut m = Malvi::new();
+
+    let mut args = ::std::env::args();
+
+    if args.len() <= 1 {
+        interactive_mode(&mut m)?;
+    } else {
+        let _ = args.next().unwrap();
+        let fa = args.next().unwrap();
+        if fa == "--help" || fa == "-?" || fa == "-h" {
+            println!("Usage: malvi [{{-c command|[-i] file}}]");
+            println!("   malvi - interactive mode");
+            println!("   malvi file.mal - interpreter mode");
+            println!("   malvi -i file.mal - load file, then go interactive");
+            println!("   malvi -c '()' - execute one thing");
+            return Ok(())
+        }
+        if fa == "-c" {
+            let na = args.next().unwrap();
+            for x in m.read(&na)? {
+                let _ = m.eval(&x)?;
+            }
+            return Ok(())
+        }
+        if fa == "-i" {
+            if args.len() == 0 {
+                interactive_mode(&mut m)?;
+                return Ok(())
+            }
+            let na = args.next().unwrap();
+            let s = ::std::fs::read_to_string(na)?;
+            for x in m.read(&s)? {
+                let _ = m.eval(&x)?;
+            }
+            interactive_mode(&mut m)?;
+            return Ok(())
+        }
+
+        let s = ::std::fs::read_to_string(fa)?;
+        for x in m.read(&s)? {
+            let _ = m.eval(&x)?;
+        }
+    }
+
     Ok(())
 }
